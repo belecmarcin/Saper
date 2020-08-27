02e30472b9d993e790d618dfd4493a8df89bd90b
belecmarcin <69787285+belecmarcin@users.noreply.github.com>
2020-08-27 16:05:37 +0200

    Field is now a Class!
    Field is now a Class!

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
                     <div className="controlPanel">
                         <p className="flag-counter">10</p>
                         <img className="flag" src="https://images.emojiterra.com/mozilla/512px/1f4a3.png" alt="flag counter"/>
-                        <a href="">
+                        <a href="#">
                             <img className="reset"
                                  src="https://cdn2.iconfinder.com/data/icons/basic-ui-elements-round/700/012_restart-2-512.png"
                                  alt="reset"/></a>
@@ -26,12 +26,14 @@ function App() {
         </div>
     );
 }
-function Field() {
-    let cells = Array.from(Array(72).keys())
-    let cellDiv = cells.map(cell => <div className="cell" key={cell}>  </div>);
-    return <div className="Field">
-        {cellDiv}
-    </div>
+class Field extends React.Component {
+    render() {
+        let cells = Array.from(Array(72).keys());
+        let cellDiv = cells.map(cell => <button className="cell" key={cell}> </button>);
+        return <div className="Field">
+            {cellDiv}
+        </div>
+    }
 }
 
 export default App;
