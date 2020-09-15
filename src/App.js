f6f744e543fa034727f26cfdba0a5919b5045991
belecmarcin <69787285+belecmarcin@users.noreply.github.com>
2020-09-16 00:22:49 +0200

    Right Click
    Right Click is now working as it should. Checking a cell is now possible. Also counter was added.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import logo from './bomb.svg';
-import mine from './fired.svg';
-import target from './target.svg'
 import './App.css';
 
 function App() {
@@ -32,61 +30,51 @@ function App() {
 class Field extends React.Component {
     constructor(props) {
         super(props);
-        //Initial state
         this.state = {
-            clicked: [],
+            leftClicked: [],
+            rightClicked: [],
+            counter: 9,
             cells: Array.from(Array(72).keys()),
-            bombs: Array.from({length: 10}, (_, i) => Math.floor(Math.random() * 72)),
-            rightClick: [],
-            increment: 0,
+            bombs: Array.from({length: 10}, (_, i) => Math.floor(Math.random() * 72))
         };
+
+        this.cell = this.cell.bind(this);
     }
 
-    cell(cell, right) {
+    cell(cell, left) {
         this.setState({
-            clicked: cell === -1 ? this.state.clicked : [ ...this.state.clicked, cell],
-            increment: this.state.increment + right,
-            rightClick: right === 0 ? this.state.rightClick : [ ...this.state.rightClick, this.state.increment],
-        })
-        console.log("clicked:", this.state.clicked);
-        console.log("right", this.state.rightClick, this.state.increment);
+            leftClicked: left ? [ ...this.state.leftClicked, cell] : this.state.leftClicked,
+            rightClicked: left ? this.state.rightClicked : [ ...this.state.rightClicked, cell],
+            counter: left ? this.state.counter : this.state.counter - 1
+        });
+        console.log(cell);
     }
 
     render() {
-        //let rightClick = false;
-        return <div className="Field" onContextMenu={(e) => {
-            e.preventDefault(); this.cell(-1, 1)}
-        }
-            onClick={(e) => { console.log("text")}}>
-            {
-                this.state.cells.map(cell => {
-                    console.log("increment", this.state.increment, this.state.rightClick);
-                    if (this.state.rightClick.includes(this.state.increment)) {
-                        return (<button className="Target-pic" key={cell}
-                                        onClick={() => this.cell(cell, 0)
-                                        }>
-                        </button>)
-                    }
-                    else if (this.state.bombs.includes(cell) && this.state.clicked.includes(cell)) {
-                        return (<button className="Mine-pic" key={cell}
-                                        onClick={() => this.cell(cell, 0)}>
-                            </button>)
-                    }
-                    else {
-                        if (this.state.clicked.includes(cell)) {
-                            console.log("jestem");
-                            return (<button className="cellClicked" key={cell}
-                                            onClick={() => this.cell(cell, 0)}>
-                            </button>)
-                        } else {
-                            return (<button className="cellNotClicked" key={cell}
-                                            onClick={() => this.cell(cell, 0)}>
-                            </button>)
-                        }
-                    }
-                })
-            }
+        return <div className="Field">
+            {this.Click()}
         </div>
+    }
+
+    Click() {
+       return this.state.cells.map(cell => this.state.leftClicked.includes(cell) && this.state.bombs.includes(cell) ?
+                <button className="bomb" key={cell}
+                        onClick={() => {this.cell(cell, true);}}>
+                </button> : this.state.leftClicked.includes(cell) ?
+                <button className="leftClick" key={cell}
+                        onClick={() => {this.cell(cell, true);}}>
+                </button> : this.state.rightClicked.includes(cell) ?
+                <button className="rightClick" key={cell}
+                        onContextMenu={(e) => {e.preventDefault(); this.cell(cell, false);}}>
+                </button> :
+                <button className="cell" key={cell}
+                        onClick={() => {this.cell(cell, true);}}
+                        onContextMenu={(e) => {e.preventDefault(); this.cell(cell, false); console.log(this.state.counter);
+                                                this.state.counter >= 0 ? 
+                                                    document.getElementsByClassName("flag-counter")[0].innerHTML = this.state.counter :
+                                                    document.getElementsByClassName("flag-counter")[0].innerHTML = this.state.counter}}
+                        >
+                </button>)
     }
 }
 
