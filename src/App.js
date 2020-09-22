7dcb1dc90072edfed872f8a3f478328faece237f
belecmarcin <69787285+belecmarcin@users.noreply.github.com>
2020-09-22 22:24:16 +0200

    Added Timer
    Timer is working but is not stopping when bomb is pressed. I implemented the buttons wrong. the functions are not working as i supposed at the beginning. I will do a new branch and start with new approach.

--- src/App.js
+++ src/App.js
@@ -18,13 +18,56 @@ function App() {
                             <img className="reset"
                                  src="https://cdn2.iconfinder.com/data/icons/basic-ui-elements-round/700/012_restart-2-512.png"
                                  alt="reset"/></a>
-                        <p className="counter">0:00</p>
+                        <Counter/>
                     </div>
                     <Field/>
                 </div>
             </header>
         </div>
     );
+}
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            seconds: 0,
+            minutes: 0,
+            isOn: false,
+        };
+
+        this.secondsTick = this.secondsTick.bind(this);
+        this.bombPressed = this.bombPressed.bind(this);
+    }
+
+    componentDidMount() {
+        document.addEventListener('mousedown', () => this.state.isOn ? null : this.secondsTick())
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.secondsID);
+    }
+
+    secondsTick() {
+        this.setState({
+            isOn: true
+        })
+        this.secondsID = setInterval(() => {
+            this.setState({
+                seconds: this.state.seconds + 1 === 60 ? 0 : this.state.seconds + 1,
+                minutes: this.state.seconds + 1 === 60 ? this.state.minutes + 1 : this.state.minutes
+            });
+        }, 1000);
+    }
+
+    bombPressed() {
+        console.log("jestem counter");
+        clearInterval(this.secondsID);
+    }
+
+    render() {
+        return <p className="counter">{this.state.minutes + ":" +('0' + this.state.seconds).slice(-2)}</p>
+    }
 }
 
 class Field extends React.Component {
@@ -39,15 +82,21 @@ class Field extends React.Component {
         };
 
         this.cell = this.cell.bind(this);
+        this.call = this.call.bind(this);
+    }
+    call() {
+        console.log("jestem");
+        const counter = new Counter();
+        counter.bombPressed();
     }
 
     cell(cell, left) {
         this.setState({
             leftClicked: left ? [ ...this.state.leftClicked, cell] : this.state.leftClicked,
             rightClicked: left ? this.state.rightClicked : [ ...this.state.rightClicked, cell],
-            counter: left ? this.state.counter : this.state.counter - 1
+            counter: left ? this.state.counter : this.state.counter - 1,
         });
-        console.log(cell);
+
     }
 
     render() {
@@ -57,9 +106,8 @@ class Field extends React.Component {
     }
 
     Click() {
-       return this.state.cells.map(cell => this.state.leftClicked.includes(cell) && this.state.bombs.includes(cell) ?
-                <button className="bomb" key={cell}
-                        onClick={() => {this.cell(cell, true);}}>
+        return this.state.cells.map(cell => this.state.leftClicked.includes(cell) && this.state.bombs.includes(cell) ?
+                <button className="bomb" key={cell}>{this.call()}
                 </button> : this.state.leftClicked.includes(cell) ?
                 <button className="leftClick" key={cell}
                         onClick={() => {this.cell(cell, true);}}>
@@ -68,11 +116,11 @@ class Field extends React.Component {
                         onContextMenu={(e) => {e.preventDefault(); this.cell(cell, false);}}>
                 </button> :
                 <button className="cell" key={cell}
-                        onClick={() => {this.cell(cell, true);}}
+                        onClick={() => { this.cell(cell, true);}}
                         onContextMenu={(e) => {e.preventDefault(); this.cell(cell, false); console.log(this.state.counter);
                                                 this.state.counter >= 0 ? 
                                                     document.getElementsByClassName("flag-counter")[0].innerHTML = this.state.counter :
-                                                    document.getElementsByClassName("flag-counter")[0].innerHTML = this.state.counter}}
+                                                    document.getElementsByClassName("flag-counter")[0].innerHTML = this.state.counter;}}
                         >
                 </button>)
     }
